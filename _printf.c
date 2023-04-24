896ccaeb8d1f92737ac912f88c657d35aa60b149
Soh <sohojiambo@outlook.com>
2023-04-24 03:43:21 -0700

    Added _printf.c

---
+++ _printf.c
@@ -0,0 +1,88 @@
+#include "main.h"
+
+/**
+ * printReference - prints an identifier
+ * @amp: the character to be printed. Comes after %
+ * @arg: argument for the identifier
+ *
+ * Return: the number of characters printed
+ */
+
+int printReference(char amp, va_list arg)
+{
+	int functionsList;
+
+	functionsList = i;
+
+	referenceLetter functions[] = {
+		{"c", printChar},
+		{"s", printstring},
+		{"d", printInteger},
+		{"i", printInteger},
+		{"u", printUnsignedInteger},
+		{"b", printUnsignedIntToBinary},
+		{"o", printOctal},
+		{"x", printhexadecimal},
+		{"X", printHEXADECIMAL},
+		{"S", printSTRING},
+		{"p", printPointer},
+		{NULL, NULL}
+	};
+
+	for (i = 0, functions[i].reference != NULL; i++)
+	{
+		if (functions[i].reference[0] == amp)
+			return (functions[i].output(arg));
+	}
+	return (0);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: the number of characters printed or
+ * -1 on failure
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+	int reference = 0, character = 0;
+	va_list arg;
+
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1;
+			character++;
+			continue;
+		}
+		if (format[i + 1] == '%')
+		{
+			write(1, "%", 1);
+			character++;
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+	}
+	reference = printReference(format[i + 1], arg);
+	if (reference == -1 || reference != 0)
+		i++;
+	if (reference > 0)
+		character += reference;
+	if (reference == 0)
+	{
+		write(1, "%", 1);
+		character++;
+	}
+	va_end(arg);
+	return (character);
+}
