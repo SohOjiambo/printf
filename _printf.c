6420c4052fc5739a2138972258bff7b0a2c5502f
vmoenga <125445625+vmoenga@users.noreply.github.com>
2023-04-25 00:28:42 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,86 +1,67 @@
+
 #include "main.h"
 
-/**
- * printReference - prints an identifier
- * @amp: the character to be printed. Comes after %
- * @arg: argument for the identifier
- *
- * Return: the number of characters printed
- */
-
-int printReference(char amp, va_list arg)
-{
-	int i;
-
-	referenceLetter functions[] = {
-		{"c", printChar},
-		{"s", printstring},
-		{"d", printInteger},
-		{"i", printInteger},
-		{"u", printUnsignedInteger},
-		{"b", printUnsignedIntToBinary},
-		{"o", printOctal},
-		{"x", printHexadecimal},
-		{"X", printHEXADECIMAL},
-		{"S", printSTRING},
-		{"p", printPointer},
-		{NULL, NULL}
-	};
-
-	for (i = 0; functions[i].reference != NULL; i++)
-	{
-		if (*(functions[i].reference) == amp)
-			return (functions[i].output(arg));
-	}
-	return (0);
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - produces output according to a format
- * @format: character string
- *
- * Return: the number of characters printed or
- * -1 on failure
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	int reference = 0, character = 0;
-	va_list arg;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(arg, format);
 	if (format == NULL)
 		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			write(1, &format[i], 1);
-			character++;
-			continue;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		if (format[i + 1] == '%')
+		else
 		{
-			write(1, "%", 1);
-			character++;
-			i++;
-			continue;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		if (format[i + 1] == '\0')
-			return (-1);
 	}
-	reference = printReference(format[i + 1], arg);
-	if (reference == -1 || reference != 0)
-		i++;
-	if (reference > 0)
-		character += reference;
-	if (reference == 0)
-	{
-		write(1, "%", 1);
-		character++;
-	}
-	va_end(arg);
-	return (character);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
